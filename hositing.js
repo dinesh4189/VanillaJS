b4d8096401a1cc390a1d57542f148ef77c0a2d7d
Dinesh <a.dinesh4189@gmail.com>
2021-05-13 17:38:26 +0530

    Update hositing.js

--- hositing.js
+++ hositing.js
@@ -18,3 +18,16 @@
       console.log('test');
   }
 }
+
+// how hoisting happens
+var a;
+console.log(a);
+{
+    a = 5;
+    console.log(a);
+    var a;
+    console.log(a);
+    a = 2;
+    console.log(a);
+}
+console.log(a);
