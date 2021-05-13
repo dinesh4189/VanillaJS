45e33d0ef15d6fae470c08c03f12c0af113b5d66
Dinesh <a.dinesh4189@gmail.com>
2021-05-13 17:37:54 +0530

    Create hositing.js

---
+++ hositing.js
@@ -0,0 +1,20 @@
+{
+  console.log(sayOtherName)
+  console.log(sayOtherName())
+  console.log(test());
+  console.log(sayName())
+  console.log(sayMyName())
+
+  var sayName = () =>{
+      console.log('hello world')
+  }
+  let sayMyName = () =>{
+      console.log('keep Going')
+  }
+  function sayOtherName(){
+      console.log('world is beautiful')
+  }
+  test = function() {
+      console.log('test');
+  }
+}
